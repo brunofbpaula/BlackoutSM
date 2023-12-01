5d5f9db4adfdd17a1de96efa5064ea6f0e0d74fc
brunofbpaula <deepcooper@outlook.com>
2023-11-30 23:27:52 -0300

    Create new forms and their layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import './globals.css'
+import SignInForm from './_auth/forms/SignInForm';
+import { Home } from './_root/pages';
+import SignUpForm from './_auth/forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
 
 export const App = () => {
   return (
     <main className='flex h-screen'>
       <Routes>
         {/*Public Routes*/}
-        <Route path='/sign-in' element={<SignInForm />}/>
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SignInForm />}/>
+          <Route path='/sign-up' element={<SignUpForm />}/>
+        </Route>
 
         {/*Private Routes*/}
         <Route index element={<Home/>}/>
