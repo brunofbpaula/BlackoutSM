802577bfc6e25400b6018af952d28fc38cdd8010
brunofbpaula <deepcooper@outlook.com>
2023-12-01 14:28:41 -0300

    Fix overwritten files

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,10 +1,56 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
+import { SignUpValidation } from "@/lib/validation"
+import { z } from "zod"
+
 
 const SignUpForm = () => {
+
+  const form = useForm<z.infer<typeof SignUpValidation>>({
+    resolver: zodResolver(SignUpValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof SignUpValidation>) {
+    console.log(values)
+  }
+
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Form {...form}>
+
+      <div className="sm:w-420 flex-center">
+        <img src="/assets/images/logo.svg" alt="logo" className="w-9 h-auto mr-2"/>
+        <h1 className="h2-bold">Blackout</h1>
+      </div>
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your username" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
