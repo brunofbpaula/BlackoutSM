b71161d2f5ad04f48b8a0110ced20aa60fde1e1d
brunofbpaula <deepcooper@outlook.com>
2023-12-01 15:08:50 -0300

    Update warning messages

--- src/lib/validation/index.tsx
+++ src/lib/validation/index.tsx
@@ -1,8 +1,8 @@
 import * as z from "zod"
 
 export const SignUpValidation = z.object({
-    name: z.string().min(2, {message: 'Too short'}),
-    username: z.string().min(5, {message: 'Too short'}).max(50),
+    name: z.string().min(2, {message: 'Name too short'}),
+    username: z.string().min(4, {message: 'Username must be at least 4 characters long'}).max(50),
     email: z.string().email(),
     password: z.string().min(6, {message: 'Password must be at least 6 characters long'})
   });
